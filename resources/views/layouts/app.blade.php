09a776e9972db83edefeaf7a494ae7f72e0d102a
andersonr7 <andergroundmsg@gmail.com>
2020-08-31 09:35:37 -0500

    modificcion pñantilla -> ccopy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -265,7 +265,7 @@
             <!-- /.content-wrapper -->
             <footer class="main-footer">
                 <!-- NO QUITAR -->
-                <strong>Tutoriales YouTube
+                <strong>Anderok
                     <div class="float-right d-none d-sm-inline-block">
                         <b>Version</b> 1.0
                     </div>
