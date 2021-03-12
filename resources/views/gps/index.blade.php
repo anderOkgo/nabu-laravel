e9eb4abd9bf7f180175f80e1c4074d79c2ba4f42
anderOkgo <andergroundmsg@gmail.com>
2021-03-12 16:04:23 -0500

    prueba comandos gps definicion comando

--- resources/views/gps/index.blade.php
+++ resources/views/gps/index.blade.php
@@ -5,20 +5,21 @@
 <div class="container <div col-md-6 mx-auto">
 
     <div class="form-group">
-        <label for="exampleInputEmail1">Contraeña anterior</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite contraseña">
+        <label  for="exampleInputEmail1">Contraeña anterior</label>
+        <input id="pass" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite contraseña">
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Contraeña Nueva</label>
-        <input id="nue" type="text" class="form-control" id="exampleInputPassword1" placeholder="Digite nueva contraseña">
+        <input id="new_pass" type="text" class="form-control" id="exampleInputPassword1" placeholder="Digite nueva contraseña">
       </div>
-      
 
-      <a id="btn_generate" class="btn btn-primary" href="sms:+573157625054"  data-clipboard-action="copy" data-clipboard-target="#nue">Generar</a>
-
-      
-      
-      
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <a id="btn_generate" class="btn btn-primary" href="sms:+573157625054"  data-clipboard-action="copy" data-clipboard-target="#nue">Generar</a>
+        </div>
+        <!-- /btn-group -->
+        <input id="nue" type="text" class="form-control">
+      </div>
 
     </div>
 
@@ -29,8 +30,28 @@
   <script>
      $(function() {
         var clipboard = new ClipboardJS('#btn_generate');
-         
      });
+
+     $(document).ready(function(){
+        $("input").keypress(function(){
+            genrete_comand();
+            
+        });
+
+        $("input").blur(function(){
+            genrete_comand();
+            
+        });
+
+        function genrete_comand() {
+            $('#nue').val( '777' +$('#pass').val() + $('#new_pass').val());
+        }
+
+    });
+
+
+    
+
 
      
 
